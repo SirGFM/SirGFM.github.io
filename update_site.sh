dd8a4eb1d120abb5e313b30be4db2bc8725bbcc9
SirGFM <gfmandaji@hotmail.com>
2017-03-16 11:35:09 -0300

    Add web game page

--- update_site.sh
+++ update_site.sh
@@ -8,6 +8,6 @@ if [ ${RV} -ne 0 ]; then
 fi
 
 # Recreate every web game page
-PYTHONPATH=${PYTHONPATH}:${PWD}/src python src/update_site.py --webgame src/game/ldjam/ld22.json --webgame src/game/ldjam/ld23.json --webgame src/game/ldjam/ld24.json --webgame src/game/ldjam/ld25.json --webgame src/game/ldjam/ld26.json --webgame src/game/ldjam/ld27.json --webgame src/game/ldjam/ld28.json --webgame src/game/ldjam/ld29.json --webgame src/game/ldjam/ld30.json --webgame src/game/ggj/ggj14.json
+PYTHONPATH=${PYTHONPATH}:${PWD}/src python src/update_site.py --webgame src/game/ldjam/ld22.json --webgame src/game/ldjam/ld23.json --webgame src/game/ldjam/ld24.json --webgame src/game/ldjam/ld25.json --webgame src/game/ldjam/ld26.json --webgame src/game/ldjam/ld27.json --webgame src/game/ldjam/ld28.json --webgame src/game/ldjam/ld29.json --webgame src/game/ldjam/ld30.json --webgame src/game/ggj/ggj14.json --webgame src/game/non-jam/laserdude.json
 exit $?
 
